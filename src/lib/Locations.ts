c8bb1e249f4faecdad12a9bd8f0226dab9f492da
Jatin <102823693+jatin-jamdagni@users.noreply.github.com>
2024-10-11 10:59:17 +0000

    location cords changed

--- src/lib/Locations.ts
+++ src/lib/Locations.ts
@@ -1,96 +1,93 @@
-
-export const locations =[
-    {
-      id: 1,
-      latitude: 28.349733,
-      longitude: 77.061750,
-      timestamp: "2024-07-23 17:57:46",
-      address: "Near Punjab National Bank in Bhondsi )168), Sohna, Gurgaon, Haryana"
-    },
-    {
-      id: 2,
-      latitude: 28.350790,
-      longitude: 77.062650,
-      timestamp: "2024-07-23 17:27:12",
-      address: "in Bhondsi )168), Sohna, Gurgaon, Haryana"
-    },
-    {
-      id: 3,
-      latitude: 28.372528,
-      longitude: 77.056496,
-      timestamp: "2024-07-23 16:27:12",
-      address: "in Badshahpur )87), Gurgaon, Haryana"
-    },
-    {
-      id: 4,
-      latitude: 28.456444,
-      longitude: 77.047870,
-      timestamp: "2024-07-23 16:12:29",
-      address: "in Sector 31, Gurgaon, Haryana"
-    },
-    {
-      id: 5,
-      latitude: 28.539158,
-      longitude: 77.117720,
-      timestamp: "2024-07-23 15:57:14",
-      address: "Near PNB ATM, Rangpuri, New Delhi"
-    },
-    {
-      id: 6,
-      latitude: 28.462420,
-      longitude: 77.295730,
-      timestamp: "2024-07-22 14:57:17",
-      address: "Near Wine Shop, Green Fields Colony, Sector 42, Faridabad, Haryana"
-    },
-    {
-      id: 7,
-      latitude: 28.519598,
-      longitude: 77.295620,
-      timestamp: "2024-07-22 14:27:36",
-      address: "Near Punjab & Sind Bank, Ali Extension, Ali, New Delhi"
-    },
-    {
-      id: 8,
-      latitude: 28.543016,
-      longitude: 77.309530,
-      timestamp: "2024-07-22 13:57:50",
-      address: "Near Kalindi Kunj Metro Station, Madanpur Khadar, New Delhi"
-    },
-    {
-      id: 9,
-      latitude: 28.575947,
-      longitude: 77.349730,
-      timestamp: "2024-07-22 13:42:11",
-      address: "Near Senior Mall, Block B, Sector 31, Noida, Uttar Pradesh"
-    },
-    {
-      id: 10,
-      latitude: 28.636631,
-      longitude: 77.316000,
-      timestamp: "2024-07-22 12:42:11",
-      address: "Near Punjab National Bank, Ghazipur Village, Ghazipur, New Delhi"
-    },
-    {
-      id: 11,
-      latitude: 28.632610,
-      longitude: 77.283270,
-      timestamp: "2024-07-22 12:27:09",
-      address: "Near Binaca Niwas & Mess, Shakarpur Extension, Shakarpur Khas, New Delhi"
-    },
-    {
-      id: 12,
-      latitude: 28.641232,
-      longitude: 77.294450,
-      timestamp: "2024-07-22 12:12:12",
-      address: "Near KFC - Preet Vihar, New Rajdhani Enclave, Swasthya Vihar, New Delhi"
-    },
-    {
-      id: 13,
-      latitude: 28.635504,
-      longitude: 77.315980,
-      timestamp: "2024-07-22 12:04:01",
-      address: "Near Rajkiya Madhyamik Vidyalaya, Ghazipur Village, Ghazipur, New Delhi"
-    }
-  ]
-  
-  
+export const locations = [
+  {
+    id: 1,
+    latitude: 28.6139,
+    longitude: 77.2090,
+    timestamp: "2024-07-23 17:57:46",
+    address: "India Gate, New Delhi"
+  },
+  {
+    id: 2,
+    latitude: 28.5355,
+    longitude: 77.2562,
+    timestamp: "2024-07-23 17:27:12",
+    address: "Red Fort, New Delhi"
+  },
+  {
+    id: 3,
+    latitude: 26.8467,
+    longitude: 80.9462,
+    timestamp: "2024-07-23 16:27:12",
+    address: "Taj Mahal, Agra"
+  },
+  {
+    id: 4,
+    latitude: 26.9124,
+    longitude: 80.9505,
+    timestamp: "2024-07-23 16:12:29",
+    address: "Agra Fort, Agra"
+  },
+  {
+    id: 5,
+    latitude: 28.6139,
+    longitude: 77.2090,
+    timestamp: "2024-07-23 15:57:14",
+    address: "Rashtrapati Bhavan, New Delhi"
+  },
+  {
+    id: 6,
+    latitude: 28.6328,
+    longitude: 77.2182,
+    timestamp: "2024-07-22 14:57:17",
+    address: "Qutub Minar, New Delhi"
+  },
+  {
+    id: 7,
+    latitude: 28.5429,
+    longitude: 77.2611,
+    timestamp: "2024-07-22 14:27:36",
+    address: "Humayun's Tomb, New Delhi"
+  },
+  {
+    id: 8,
+    latitude: 28.6448,
+    longitude: 77.2179,
+    timestamp: "2024-07-22 13:57:50",
+    address: "Lotus Temple, New Delhi"
+  },
+  {
+    id: 9,
+    latitude: 28.6448,
+    longitude: 77.2179,
+    timestamp: "2024-07-22 13:42:11",
+    address: "Akshardham Temple, New Delhi"
+  },
+  {
+    id: 10,
+    latitude: 28.4595,
+    longitude: 77.0279,
+    timestamp: "2024-07-22 12:42:11",
+    address: "Gurudwara Bangla Sahib, New Delhi"
+  },
+  {
+    id: 11,
+    latitude: 28.6123,
+    longitude: 77.2066,
+    timestamp: "2024-07-22 12:27:09",
+    address: "National Museum, New Delhi"
+  },
+  {
+    id: 12,
+    latitude: 28.5244,
+    longitude: 77.1899,
+    timestamp: "2024-07-22 12:12:12",
+    address: "Jama Masjid, New Delhi"
+  },
+  {
+    id: 13,
+    latitude: 28.5625,
+    longitude: 77.2492,
+    timestamp: "2024-07-22 12:04:01",
+    address: "Purana Qila, New Delhi"
+  }
+];
