ed2db43021ea7a1bffefd972f17f097709d65f74
Jatin <102823693+jatin-jamdagni@users.noreply.github.com>
2024-10-11 11:06:20 +0000

    hahaha taj mahaj was sent to lucknow

--- src/lib/Locations.ts
+++ src/lib/Locations.ts
@@ -1,92 +1,92 @@
 export const locations = [
   {
     id: 1,
-    latitude: 28.6139,
-    longitude: 77.2090,
+    latitude: 28.6129,
+    longitude: 77.2295,
     timestamp: "2024-07-23 17:57:46",
     address: "India Gate, New Delhi"
   },
   {
     id: 2,
-    latitude: 28.5355,
-    longitude: 77.2562,
+    latitude: 28.6562,
+    longitude: 77.2410,
     timestamp: "2024-07-23 17:27:12",
     address: "Red Fort, New Delhi"
   },
   {
     id: 3,
-    latitude: 26.8467,
-    longitude: 80.9462,
+    latitude: 27.1751,
+    longitude: 78.0421,
     timestamp: "2024-07-23 16:27:12",
     address: "Taj Mahal, Agra"
   },
   {
     id: 4,
-    latitude: 26.9124,
-    longitude: 80.9505,
+    latitude: 27.1795,
+    longitude: 78.0211,
     timestamp: "2024-07-23 16:12:29",
     address: "Agra Fort, Agra"
   },
   {
     id: 5,
-    latitude: 28.6139,
-    longitude: 77.2090,
+    latitude: 28.6143,
+    longitude: 77.1996,
     timestamp: "2024-07-23 15:57:14",
     address: "Rashtrapati Bhavan, New Delhi"
   },
   {
     id: 6,
-    latitude: 28.6328,
-    longitude: 77.2182,
+    latitude: 28.5244,
+    longitude: 77.1855,
     timestamp: "2024-07-22 14:57:17",
     address: "Qutub Minar, New Delhi"
   },
   {
     id: 7,
-    latitude: 28.5429,
-    longitude: 77.2611,
+    latitude: 28.5933,
+    longitude: 77.2507,
     timestamp: "2024-07-22 14:27:36",
     address: "Humayun's Tomb, New Delhi"
   },
   {
     id: 8,
-    latitude: 28.6448,
-    longitude: 77.2179,
+    latitude: 28.5535,
+    longitude: 77.2588,
     timestamp: "2024-07-22 13:57:50",
     address: "Lotus Temple, New Delhi"
   },
   {
     id: 9,
-    latitude: 28.6448,
-    longitude: 77.2179,
+    latitude: 28.6127,
+    longitude: 77.2773,
     timestamp: "2024-07-22 13:42:11",
     address: "Akshardham Temple, New Delhi"
   },
   {
     id: 10,
-    latitude: 28.4595,
-    longitude: 77.0279,
+    latitude: 28.6269,
+    longitude: 77.2134,
     timestamp: "2024-07-22 12:42:11",
     address: "Gurudwara Bangla Sahib, New Delhi"
   },
   {
     id: 11,
-    latitude: 28.6123,
-    longitude: 77.2066,
+    latitude: 28.6118,
+    longitude: 77.2195,
     timestamp: "2024-07-22 12:27:09",
     address: "National Museum, New Delhi"
   },
   {
     id: 12,
-    latitude: 28.5244,
-    longitude: 77.1899,
+    latitude: 28.6507,
+    longitude: 77.2334,
     timestamp: "2024-07-22 12:12:12",
     address: "Jama Masjid, New Delhi"
   },
   {
     id: 13,
-    latitude: 28.5625,
-    longitude: 77.2492,
+    latitude: 28.6092,
+    longitude: 77.2433,
     timestamp: "2024-07-22 12:04:01",
     address: "Purana Qila, New Delhi"
   }
